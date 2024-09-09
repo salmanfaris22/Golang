97fac5da93e0088add8583d017292679ef8d9689
salmanfaris22 <salman567633800@gmail.com>
2024-09-09 11:42:40 +0530

    Golang

--- 07_slicec/main.go
+++ 07_slicec/main.go
@@ -1,21 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 
 	// var nums []int
 
-	var nums = make([]int, 3, 5)
+	// var nums = make([]int, 3, 5)
 	// nums = append(nums, 3)
 	// nums = append(nums, 3)
 	// nums = append(nums, 3)
 	// nums = append(nums, 3)
 
 	// nums := []int{}
-	nums[0] = 2
-	fmt.Println(nums)
-	fmt.Println(cap(nums))
-	fmt.Println(len(nums))
+	// nums[0] = 2
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// fmt.Println(len(nums))
+
+	// var num1 = make([]int, 0, 5)
+	// num1 = append(num1, 2)
+	// var num2 = make([]int, len(num1))
+	// fmt.Println(num2, num1)
+	// copy(num2, num1)
+	// fmt.Println(num2, num1)
+
+	// var nums = []int{1, 2, 3, 4, 5, 6, 7}
+	// fmt.Println(nums[:4])
+
+	var num1 = []int{1, 2}
+	var num2 = []int{1, 2}
+	fmt.Println(slices.Equal(num1, num2))
 
 }
